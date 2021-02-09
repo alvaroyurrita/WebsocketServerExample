458b29855c937d24871b440b83122a62ebac80e0
Alvaro Yurrita <alvro.yurrita@jenandal.com>
2021-02-09 07:14:12 -0500

    Bidirectional Control Established

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,14 +1,30 @@
-import * as CrComLib from "./cr-com-lib";
+import * as CrComLib from "@crestron/ch5-crcomlib";
+import { bridgeReceiveIntegerFromNative, bridgeReceiveBooleanFromNative, bridgeReceiveStringFromNative, bridgeReceiveObjectFromNative } 
+    from '@crestron/ch5-crcomlib/build_bundles/cjs/cr-com-lib';
+window['bridgeReceiveIntegerFromNative'] = bridgeReceiveIntegerFromNative;
+window['bridgeReceiveBooleanFromNative'] = bridgeReceiveBooleanFromNative;
+window['bridgeReceiveStringFromNative'] = bridgeReceiveStringFromNative;
+window['bridgeReceiveObjectFromNative'] = bridgeReceiveObjectFromNative;
 
 let buttonState = false;
 let buttonValue = 0;
 
 const button=document.querySelector(".demo");
 
-CrComLib.subscribeState("n","12",(value)=>{
-    document.getElementById('theSlider').value = value;
+
+CrComLib.subscribeState("n",11,(val)=>{
+    console.log(`received value 11 ${val}`);
 })
 
+CrComLib.subscribeState("b",16,(state)=>{
+    console.log(`received b-16 state ${state}`);
+    if (state){
+        button.className="demoActive";
+    }
+    else{
+        button.className="demo";
+    }
+})
 
 button.addEventListener("click",() =>{
     buttonState = !buttonState
