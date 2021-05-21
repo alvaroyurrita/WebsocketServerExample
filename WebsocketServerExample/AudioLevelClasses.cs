1f8b5a47f1f6e0cf4f2f979ac2cc80079e41cfa3
Alvaro Yurrita <alvaro.yurrita@jenandal.com>
2021-05-21 15:03:52 -0400

    Add project files.

---
+++ WebsocketServerExample/AudioLevelClasses.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace WebsocketServerExample
+{
+    public class AudioLevelMessage
+    {
+        public string message = "";
+        public List<AudioLevel> audioLevels = new List<AudioLevel>() { new AudioLevel("") };
+
+        public AudioLevelMessage(string msg)
+        {
+            this.message = msg;
+        }
+    }
+
+    public class AudioLevel
+    {
+        [JsonProperty("name")]
+        public string Name { get; }
+        [JsonProperty("levelPercent")]
+        public int LevelPercent { get; set; }
+        [JsonProperty("isMuted")]
+        public bool IsMuted { get; set; }
+
+        public AudioLevel(string Name = "", int LevelPercent = 80, bool IsMuted = false)
+        {
+            this.Name = Name;
+            this.LevelPercent = LevelPercent;
+            this.IsMuted = IsMuted;
+        }
+
+    }
+}
---
+++ WebsocketServerExample/WebSocketHelper.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Crestron.SimplSharpPro;                       	// For Basic SIMPL#Pro classes
+using Crestron.SimplSharp;                          	// For Basic SIMPL# Classes
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Authentication;
+using Fleck;
+
+namespace WebsocketServerExample
+{
+    public class WebSocketHelper
+    {
+        List<AudioLevel> RoomAudioLevels;
+        List<IWebSocketConnection> allSockets = new List<IWebSocketConnection>();
+
+        public WebSocketHelper(List<AudioLevel> RoomAudioLevels)
+        {
+            this.RoomAudioLevels = RoomAudioLevels;
+            var Server = new WebSocketServer("wss://192.168.0.143:4649");
+            Server.EnabledSslProtocols = SslProtocols.Tls12 | SslProtocols.None | SslProtocols.Default;
+            Server.Certificate = new X509Certificate2("./Certificate/webserver_cert.pfx", "DGItech01862!");
+            Server.SupportedSubProtocols = new[] { "Audio" };
+            Server.Start(socket =>
+            {
+                CrestronConsole.PrintLine("WebSocket started");
+
+                socket.OnError = obj =>
+                {
+                    CrestronConsole.PrintLine(obj.Message);
+                };
+
+                socket.OnOpen = () =>
+                {
+                    CrestronConsole.PrintLine("Socket Open");
+                    CrestronConsole.PrintLine($"{socket.ConnectionInfo.Id}");
+                    allSockets.Add(socket);
+                };
+
+                socket.OnClose = () =>
+                {
+                    CrestronConsole.PrintLine("Socket Closed");
+                    CrestronConsole.PrintLine($"{socket.ConnectionInfo.Id}");
+                    allSockets.Remove(socket);
+                };
+
+                socket.OnMessage = Message =>
+                {
+                    var msg = JsonConvert.DeserializeObject<AudioLevelMessage>(Message);
+                    switch (msg.message)
+                    {
+                        case "GET":
+                            socket.Send(JsonConvert.SerializeObject(RoomAudioLevels));
+                            break;
+                        case "POST":
+                            RoomAudioLevels = msg.audioLevels;
+                            //socket.Send(JsonConvert.SerializeObject(RoomAudioLevels));
+                            allSockets.ForEach(c => c.Send(JsonConvert.SerializeObject(RoomAudioLevels)));
+                            break;
+                        default:
+                            break;
+                    }
+                };
+            });
+        }
+
+        public void SendUpdate()
+        {
+            var message = JsonConvert.SerializeObject(RoomAudioLevels);
+            allSockets.ForEach(c => c.Send(message));
+        }
+    }
+}
